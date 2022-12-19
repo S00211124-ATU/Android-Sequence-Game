b700b215e29cb578f4ed9a2bcaf28a5e34eeabd2
ohnoSnoopy <S00211124@mail.itsligo.ie>
2022-12-19 19:39:15 +0000

    first commit

---
+++ app/src/main/java/gameproject/app/MainActivity.java
@@ -0,0 +1,28 @@
+package gameproject.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        playButton = findViewById(R.id.playButton);
+    }
+
+    public void goPlay(View view)
+    {
+        startActivity(new Intent(MainActivity.this, PlayScreen.class));
+    }
+}
---
+++ app/src/main/java/gameproject/app/PlayScreen.java
@@ -0,0 +1,136 @@
+package gameproject.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class PlayScreen extends AppCompatActivity {
+
+    private final int BLUE = 1;
+    private final int RED = 2;
+    private final int YELLOW = 3;
+    private final int GREEN = 4;
+
+    Button redButton, blueButton, yellowButton, greenButton, fb;
+    int sequenceCount = 4, n = 0;
+    private Object mutex = new Object();
+    int[] gameSequence = new int[120];
+    int arrayIndex = 0;
+
+    CountDownTimer ct = new CountDownTimer(6000,  1500) {
+
+        public void onTick(long millisUntilFinished) {
+            //mTextField.setText("seconds remaining: " + millisUntilFinished / 1500);
+            oneButton();
+            //here you can have your logic to set text to edittext
+        }
+
+        public void onFinish() {
+            //mTextField.setText("done!");
+            // we now have the game sequence
+
+            for (int i = 0; i< arrayIndex; i++)
+                Log.d("game sequence", String.valueOf(gameSequence[i]));
+            // start next activity
+
+            // put the sequence into the next activity
+            // stack overglow https://stackoverflow.com/questions/3848148/sending-arrays-with-intent-putextra
+            //Intent i = new Intent(A.this, B.class);
+            //i.putExtra("numbers", array);
+            //startActivity(i);
+
+            // start the next activity
+            // int[] arrayB = extras.getIntArray("numbers");
+        }
+    };
+
+    public void doPlay(View view) {
+        ct.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_screen);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        redButton = findViewById(R.id.redButton);
+        yellowButton = findViewById(R.id.yellowButton);
+        greenButton = findViewById(R.id.greenButton);
+        blueButton = findViewById(R.id.blueButton);
+    }
+
+    private int getRandom(int maxValue) {
+        return ((int) ((Math.random() * maxValue) + 1));
+    }
+
+    private void oneButton() {
+        n = getRandom(sequenceCount);
+
+        switch (n) {
+            case 1:
+                flashButton(blueButton);
+                gameSequence[arrayIndex++] = BLUE;
+                break;
+            case 2:
+                flashButton(redButton);
+                gameSequence[arrayIndex++] = RED;
+                break;
+            case 3:
+                flashButton(yellowButton);
+                gameSequence[arrayIndex++] = YELLOW;
+                break;
+            case 4:
+                flashButton(greenButton);
+                gameSequence[arrayIndex++] = GREEN;
+                break;
+            default:
+                break;
+        }   // end switch
+    }
+
+    private void flashButton(Button button) {
+        fb = button;
+        Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            public void run() {
+
+                fb.setPressed(true);
+                fb.invalidate();
+                fb.performClick();
+                Handler handler1 = new Handler();
+                Runnable r1 = new Runnable() {
+                    public void run() {
+                        fb.setPressed(false);
+                        fb.invalidate();
+                    }
+                };
+                handler1.postDelayed(r1, 600);
+
+            } // end runnable
+        };
+        handler.postDelayed(r, 600);
+    }
+
+    public void doTest(View view) {
+        for (int i = 0; i < sequenceCount; i++) {
+            int x = getRandom(sequenceCount);
+
+            if (x == 1)
+                flashButton(blueButton);
+            else if (x == 2)
+                flashButton(redButton);
+            else if (x == 3)
+                flashButton(yellowButton);
+            else if (x == 4)
+                flashButton(greenButton);
+        }
+    }
+}
